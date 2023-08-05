226236765f4105e8ec10c7dc4b08e66627ee922c
Драган Ћајић <cajic_dragan@yahoo.com>
2023-08-05 22:52:46 +0200

    ~ create template
    > create `index` template:
      (blog-project -> blog_app -> templates -> blog_app -> index.html)
    > register your app to find template(s):
      (blog-project -> blog -> settings.py -> `INSTALLED_APPS = [ 'blog_app',]`)
    
    > Django's Double-Folder Structure:
    
    app_name
    └── templates
        └── app_name
            ├── template1.html
            └── template2.html

--- blog-project/blog_app/views.py
+++ blog-project/blog_app/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    #pass
-    #return None
-
-    # return HttpResponse OBJECT or Error!
-    return HttpResponse(
-"""<!DOCTYPE html>
-<html>
-    <body>
-        <h1>This is my Blog!</h1>
-        <p>Welcome to <em>my blog</em>!</p>
-        <p>If you want, learn more about me
-            <a href="https://learn-pisio.eu5.org/" target="_blank">here</a>
-        </p>
-    </body>
-</html>""")
+    return render(request, 'blog_app/index.html')
