45d7f248a4ebe68b3b2428196298149ff4bb42d2
Драган Ћајић <cajic_dragan@yahoo.com>
2023-08-09 23:14:35 +0200

    write first model
    > 5.1 Creating And Managing Your Data
    > ~-~-~ 5.1.3 Write a Model ~-~-~ ;-)

--- blog-project/blog_app/models.py
+++ blog-project/blog_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# name of Model Class: capitalized AND in singular
+class Post(models.Model):
+    title = models.CharField(max_length=140)
+    text = models.TextField()
+    published = models.DateTimeField(auto_now_add=True)
