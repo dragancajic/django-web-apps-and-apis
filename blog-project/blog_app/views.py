88e38cd541a6bf4ea0ec88bad838842dceffd8fc
Драган Ћајић <cajic_dragan@yahoo.com>
2023-08-13 22:52:14 +0200

    pull information from database
    > firstly,
    
    // [make migrations and] apply/run ALL migrations for `default Django apps`
    $ python3.10 manage.py makemigrations
    $ python3.10 manage.py makemigrations [blog_app]
    
    $ python3.10 manage.py migrate
    $ python3.10 manage.py migrate [blog_app]
    
    > then, use
    
    ~ DJANGO SHELL
    
    o Database Access Through Django o
    
    - Access and interact with your database backend through the Django ORM (Object-Relational Mapper)
    using the Django Shell.
    
    Django ORM in Shell (just Django-aware standard Python shell!)
    
    $ python3.10 manage.py shell
    
    // e.g.
    (CodingNomads) dragancajic@localhost:~/Devs/CodingNomads/blog-project> python3.10 manage.py shell
    
    [this is] the interaction with the database:
    
    // save to the database
    p.save()
    
    // making a call to the database:
    // fetching all objects that are currently in the Post table of database
    Post.objects.all()
    
    > AFTER all this, in `views` create context dictionary that is filled with
      query object values AND use reference in template to pull information.

--- blog-project/blog_app/views.py
+++ blog-project/blog_app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Post
+# '.' is the shortcut for current app, instead of writing
+#from blog_app.models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog_app/index.html')
+    # in views you can put some logic,
+    # e.g. make a call to the database ;-)
+
+    # posts - query object that contains all posts pulled from the database
+    posts = Post.objects.all()
+    print(posts)
+
+    # create context dictionary to fill with query object values
+    # context_dict = {'key': query_object}
+    context = {'posts': posts}
+
+    for key, value in context.items():
+        print(f"key: {key}, value: {value}")
+
+    # pass context dictionary as third argument to the render function
+    return render(request, 'blog_app/index.html', context)
+
+    # Now, `index` template has access to the information saved into context dictionary.
